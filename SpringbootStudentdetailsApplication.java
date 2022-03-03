9b4806f5e9c58b92aa45b4cc9b80c9590a4b7c0a
soumyadeep91 <99657084+soumyadeep91@users.noreply.github.com>
2022-03-03 12:17:55 +0530

    Add files via upload

---
+++ SpringbootStudentdetailsApplication.java
@@ -0,0 +1,13 @@
+package com.soumya.springbbot.springbootstudentdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentdetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudentdetailsApplication.class, args);
+	}
+
+}
